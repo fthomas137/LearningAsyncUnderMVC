3b438647df3d991f398a174e74d290caaa5f321b
Frank Thomas <FTHOMAS@calgary.ca>
2020-01-24 11:32:24 -0700

    Actions only in Controller class

--- LearningAsyncUnderMVC/Controllers/DepartmentController.cs
+++ LearningAsyncUnderMVC/Controllers/DepartmentController.cs
@@ -65,5 +65,42 @@ namespace LearningAsyncUnderMVC.Controllers
             }
             return View(department);
         }
+
+        public async Task<ActionResult> Delete(int id = 0)
+        {
+            var department = await _db.Departments.FirstOrDefaultAsync(d => d.Id == id);
+            if (department == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            return View(department);
+        }
+
+        //[HttpPost,ActionName("Delete")] //one alternative on the delete confirmation
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> DeleteConfirmed(Department department)
+        //{
+        //    _db.Entry(department).State = EntityState.Deleted;
+        //    await _db.SaveChangesAsync();
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost, ActionName("Delete")] //second way to perform the same delete confirmation action
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _db.Departments.FirstOrDefaultAsync(d => d.Id == id);
+            _db.Departments.Remove(department);
+            await _db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
