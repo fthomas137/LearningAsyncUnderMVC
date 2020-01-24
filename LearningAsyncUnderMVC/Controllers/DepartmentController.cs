a1ea0a693f3c392142ef41351b2bd228317627d3
Frank Thomas <FTHOMAS@calgary.ca>
2020-01-24 11:13:16 -0700

    Add project files.

---
+++ LearningAsyncUnderMVC/Controllers/DepartmentController.cs
@@ -0,0 +1,69 @@
+﻿using LearningAsyncUnderMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningAsyncUnderMVC.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private MyDB _db = new MyDB();
+
+        public async Task<ActionResult> Index()
+        {
+            return View(await _db.Departments.ToListAsync());
+        }
+
+        public ActionResult Create()
+        {
+            return View(new Department());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(Department model)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Departments.Add(model);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public async Task<ActionResult> Details(int id = 0)
+        {
+            var department = await _db.Departments.FirstOrDefaultAsync(d => d.Id == id);
+            if (department == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            return View(department);
+        }
+
+        public async Task<ActionResult> Edit(int id = 0)
+        {
+            var department = await _db.Departments.FirstOrDefaultAsync(d => d.Id == id);
+            if (department == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            return View(department);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(Department department)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(department).State = EntityState.Modified;
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(department);
+        }
+    }
+}
---
+++ LearningAsyncUnderMVC/Migrations/202001241807253_ChangedDepartmentValidation.cs
@@ -0,0 +1,20 @@
+﻿namespace LearningAsyncUnderMVC.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ChangedDepartmentValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Departments", "Name", c => c.String(nullable: false));
+            AlterColumn("dbo.Departments", "Location", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Departments", "Location", c => c.String());
+            AlterColumn("dbo.Departments", "Name", c => c.String());
+        }
+    }
+}
